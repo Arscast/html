d9a2c505e78157d607aa01b57ea45ce5580b1d8c
Arseni Rakutska <r.ars1.ar@gmail.com>
2017-03-16 21:05:56 +0100

    add javascript file

---
+++ html/ex_js.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>my_first_page</title>>
+<body>
++<h1>My First JavaScript</h1>
+ 
+ <p>JavaScript can change the content of an HTML element:</p>
+ 
+ <button type="button" onclick="myFunction()">Click Me!</button>
+ 
+ <p id="demo">This is a demonstration.</p>
+ 
+ <script>
+ function myFunction() { 
+     document.getElementById("demo").innerHTML = "Hello JavaScript!";
+ }
+ </script>
+</body>	
+</html>
+
